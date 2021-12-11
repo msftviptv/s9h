cfcbc825a2e5905759b33686b032aee4c719f6c3
msftviptv <64074648+msftviptv@users.noreply.github.com>
2021-12-11 12:31:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <html xmlns:b="http://www.google.com/2005/gml/b" xmlns:data="http://www.google.com/2005/gml/data" xmlns:expr="http://www.google.com/2005/gml/expr" xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<link href="https://www.blogger.com/static/v1/widgets/4271437141-widget_css_bundle_rtl.css" rel="stylesheet" type="text/css"></link>
+
+<style type="text/css">
+/*<![CDATA[*/
+    <!-- /*<style id='page-skin-1' type='text/css'><!--
+    */
+    --></style><style>#99288_c_452241{width:100%;height:100%;position:absolute;z-index:2;top:0;right:0;left:0;bottom:0}#video{width:100%;height:100%;position:absolute;z-index:2;top:0;right:0;left:0;bottom:0}*{margin:0;padding:0}body{background:none repeat scroll 0 0 #000;margin:0;font-family:'Proxima Nova Soft','Helvetica Neue',sans-serif;}#serv-wrap{margin:0}.comments,#comments,.breadcrumbs,h1,h2{display:none}.post{margin:0;padding:0}#tivi{border-radius: 3px;height: 0;overflow: hidden;padding-bottom: 63%;padding-top: 0;}#tivi iframe, #tivi embed, #tivi object, #tivi video{border: 0 none;height: 100%;left: 0;position: absolute;top: 0;width: 100%;}#tv{height:0;overflow:hidden;padding-top:0}#tv iframe,#tv embed,#tv object,#tv video{background:#000;border:0 none;height:100%;left:0;position:absolute;top:0;width:100%}#tvdesk{border-radius:3px;height:0;overflow:hidden;padding-bottom:0;padding-top:0}#tvdesk iframe,#tv embed,#tv object,#tv video{border:0 none;height:100%;left:0;position:absolute;top:0;width:100%}#tvmob{height:0;overflow:hidden;padding-bottom:65%;padding-top:0;position:relative;display:none}#tvmob iframe,#tvmob embed,#tvmob object,#tvmob video{border:0 none;height:100%;left:0;position:absolute;top:0;width:100%}#tvtom{border:0 solid #dfdfdf;border-radius:1px;color:#6e6e6e;font-size:12px;line-height:20px;padding:10px 0 0 11px;position:absolute;z-index:999;top:0;left:0}.tvtombtn{background: #3498db linear-gradient(to bottom, #3498db, #2980b9) repeat scroll 0 0;border-radius: 2px;color: #fff;font-family: Arial;font-size: 12px;padding: 2px;text-decoration: none;}.tvtombtn:hover{background: #3cb0fd linear-gradient(to bottom, #3cb0fd, #3498db) repeat scroll 0 0;color: #fff;text-decoration: none;}.tvtombol{background: #cc0000 linear-gradient(to bottom, #3498db, #2980b9) repeat scroll 0 0;border-radius: 2px;color: #fff;font-family: Arial;font-size: 12px;padding: 2px;text-decoration: none;}._1p6f{display: block;width: auto;}._1p6g{height: auto;}._1p6h{height: 100%;width: auto;}._523u{display: inline;}._2gn_{background-color: #1d2129;color: #fff;padding-bottom: 64.25%;position: relative;text-align: center;text-shadow: 1px 1px 0 rgba(20, 22, 26, 0.3);}._2go0{bottom: 0;height: 100px;left: 0;margin: auto;position: absolute;right: 0;top: 0;width: 80%;}._2go6{font-size: 14px;margin: 16px auto;width: 180px;}._2go6 a, ._2go6 a:hover{font-weight: bold;color: #fff;text-decoration: none;}._3i0o{font-size: 24px;}._3i0p{font-size: 16px;font-weight: normal;}#player{width:100%;height:100%;position:absolute;z-index:2;top:0;right:0;left:0;bottom:0}#video{width:100%;height:100%;position:absolute;z-index:2;top:0;right:0;left:0;bottom:0}.tomboltv{background: #f3f3f3 none repeat scroll 0 0;border: 1px solid #cbc5c5;border-radius: 0.3em;color: #000;display: inline-block;font: bold 11px Arial,sans-serif;margin: 4px;min-width: 80px;padding: 5px 3px;text-align: center;text-decoration: none;}.tomboltv:hover{border-color: #cbc5c5;box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);color: #000;}#live{font-size:12px;line-height:20px;position:absolute;top:0;left:0;background-color:#;border:0 solid #DFDFDF;border-radius:1px;padding:1px 2px 2px;color:#6E6E6E;z-index:999}.livebtn{color:#fff;background:#3498db;background-image:-webkit-linear-gradient(top,#3498db,#2980b9);background-image:-moz-linear-gradient(top,#3498db,#2980b9);background-image:-ms-linear-gradient(top,#3498db,#2980b9);background-image:-o-linear-gradient(top,#3498db,#2980b9);background-image:linear-gradient(to bottom,#3498db,#2980b9);-webkit-border-radius:2;-moz-border-radius:4;border-radius:2px;font-family:Arial;color:#fff;font-size:12px;padding:2px;text-decoration:none}.livebtn:hover{background:#3cb0fd;background-image:-webkit-linear-gradient(top,#3cb0fd,#3498db);background-image:-moz-linear-gradient(top,#3cb0fd,#3498db);background-image:-ms-linear-gradient(top,#3cb0fd,#3498db);background-image:-o-linear-gradient(top,#3cb0fd,#3498db);background-image:linear-gradient(to bottom,#3cb0fd,#3498db);text-decoration:none}#menuz{width:100%;margin-top:10px;}#menuz li{text-align:center;font-size:20px;background:#005b9a;list-style: none;margin: 0px 0px 5px 0px;padding: 4px 8px 4px 8px; border:1px solid #0191c8;border-radius: 5px;}#menuz li a, #menuz li:hover{text-decoration: none;color:#fff}.tombut{display: inline-block;min-width: 100%;text-decoration: none;}.tombut:hover{color:#fff;}
+.tomboltvv {background: #f3f3f3 none repeat scroll 0 0;border: 1px solid #cbc5c5;border-radius: 0.3em;color: #000;display: inline-block;font: bold 11px Arial,sans-serif;margin: 0;min-width: 50px;padding: 5px 3px;text-align: center;text-decoration: none;}.tomboltvv:hover {border-color: #cbc5c5;box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);color: #000;}
+#tvmob{height:0;overflow:hidden;padding-bottom:65%;padding-top:0;position:relative;display:none}#tvmob iframe,#tvmob embed,#tvmob object,#tvmob video{border:0 none;height:100%;left:0;position:absolute;top:0;width:100%}#tvtom{border:0 solid #dfdfdf;border-radius:1px;color:#6e6e6e;font-size:12px;line-height:20px;padding:10px 0 0 11px;position:absolute;z-index:999;top:0;left:0}#player{width:100%;height:100%;position:absolute;z-index:2;top:0;right:0;left:0;bottom:0}
+<div id='web'>
+<\/div><\/style>
+    <\/head><!--<head>
+    <link href='https://www.blogger.com/static/v1/widgets/2549344219-widget_css_bundle.css' rel='stylesheet' type='text/css'/>
+<link href='https://www.blogger.com/dyn-css/authorization.css?targetBlogID=9052138879193408892&amp;zx=a5374f81-3873-45e1-aaa9-17e13a7fe66c' media='none' onload='if(media!=&#39;all&#39;)media=&#39;all&#39;' rel='stylesheet'/><noscript><link href='https://www.blogger.com/dyn-css/authorization.css?targetBlogID=9052138879193408892&amp;zx=a5374f81-3873-45e1-aaa9-17e13a7fe66c' rel='stylesheet'/></noscript>
+    <\/head>-->
+<body class='loading'><div id='serv-wrap'>
+<div class='main section' id='main'>
+<div class='widget HTML' data-version='1' id='HTML1'>
+    <\/div>
+<!--
+    <script type="text/javascript" src="https://www.blogger.com/static/v1/widgets/609130596-widgets.js"><\/script>
+    <script type='text/javascript'>
+    window['__wavt'] = 'AOuZoY4y4FbQCx821sX4qKBpWHvut2UhCw:1546823001813';_WidgetManager._Init('//www.blogger.com/rearrange?blogID\9052138879193408892','','9052138879193408892');
+    _WidgetManager._SetDataContext([{'name': 'blog', 'data': {'blogId': '9052138879193408892', 'title': '', 'url': 'http://www.beinlive4u.net', 'canonicalUrl': 'http://www.beinlive4u.net/', 'homepageUrl': 'http://www.beinlive4u.net/', 'searchUrl': 'http://www.beinlive4u.net', 'canonicalHomepageUrl': '', 'blogspotFaviconUrl': 'http://www.beinlive4u.net/favicon.ico', 'bloggerUrl': 'https://www.blogger.com', 'hasCustomDomain': true, 'httpsEnabled': false, 'enabledCommentProfileImages': true, 'gPlusViewType': 'FILTERED_POSTMOD', 'adultContent': false, 'analyticsAccountNumber': '', 'encoding': 'UTF-8', 'locale': 'id', 'localeUnderscoreDelimited': 'id', 'languageDirection': 'ltr', 'isPrivate': false, 'isMobile': false, 'isMobileRequest': false, 'mobileClass': '', 'isPrivateBlog': false, 'feedLinks': '\x3clink rel\x3d\x22alternate\x22 type\x3d\x22application/atom+xml\x22 title\x3d\x22 - Atom\x22 href\x3d\x22\x22 /\x3e\n\x3clink rel\x3d\x22alternate\x22 type\x3d\x22application/rss+xml\x22 title\x3d\x22 - RSS\x22 href\x3d\x22http://www.beinlive4u.net/feeds/posts/default?alt\x3drss\x22 /\x3e\n\x3clink rel\x3d\x22service.post\x22 type\x3d\x22application/atom+xml\x22 title\x3d\x22 - Atom\x22 href\x3d\x22https://www.blogger.com/feeds/9052138879193408892/posts/default\x22 /\x3e\n', 'meTag': '', 'adsenseHostId': '', 'adsenseHasAds': false, 'view': '', 'dynamicViewsCommentsSrc': '//www.blogblog.com/dynamicviews/4224c15c4e7c9321/js/comments.js', 'dynamicViewsScriptSrc': '//www.blogblog.com/dynamicviews/0e9be7d874b530da', 'plusOneApiSrc': 'https://apis.google.com/js/plusone.js', 'sharing': {'platforms': [{'name': 'Dapatkan link', 'key': 'link', 'shareMessage': 'Dapatkan link', 'target': ''}, {'name': 'Facebook', 'key': 'facebook', 'shareMessage': 'Bagikan ke Facebook', 'target': 'facebook'}, {'name': 'BlogThis!', 'key': 'blogThis', 'shareMessage': 'BlogThis!', 'target': 'blog'}, {'name': 'Twitter', 'key': 'twitter', 'shareMessage': 'Bagikan ke Twitter', 'target': 'twitter'}, {'name': 'Pinterest', 'key': 'pinterest', 'shareMessage': 'Bagikan ke Pinterest', 'target': 'pinterest'}, {'name': 'Google+', 'key': 'googlePlus', 'shareMessage': 'Bagikan ke Google+', 'target': 'googleplus'}, {'name': 'Email', 'key': 'email', 'shareMessage': 'Email', 'target': 'email'}], 'googlePlusShareButtonWidth': 300, 'googlePlusBootstrap': '\x3cscript type\x3d\x22text/javascript\x22\x3ewindow.___gcfg \x3d {\x27lang\x27: \x27id\x27};\x3c/script\x3e'}, 'hasCustomJumpLinkMessage': false, 'jumpLinkMessage': 'Baca selengkapnya', 'pageType': 'index', 'pageName': '', 'pageTitle': 'www.beinlive4u.net'}}, {'name': 'features', 'data': {'sharing_get_link_dialog': 'true', 'sharing_native': 'false'}}, {'name': 'messages', 'data': {'edit': 'Edit', 'linkCopiedToClipboard': 'Tautan disalin ke papan klip!', 'ok': 'Oke', 'postLink': 'Tautan Pos'}}, {'name': 'template', 'data': {'name': 'custom', 'localizedName': 'Khusus', 'isResponsive': false, 'isAlternateRendering': false, 'isCustom': true}}, {'name': 'view', 'data': {'classic': {'name': 'classic', 'url': '?view\x3dclassic'}, 'flipcard': {'name': 'flipcard', 'url': '?view\x3dflipcard'}, 'magazine': {'name': 'magazine', 'url': '?view\x3dmagazine'}, 'mosaic': {'name': 'mosaic', 'url': '?view\x3dmosaic'}, 'sidebar': {'name': 'sidebar', 'url': '?view\x3dsidebar'}, 'snapshot': {'name': 'snapshot', 'url': '?view\x3dsnapshot'}, 'timeslide': {'name': 'timeslide', 'url': '?view\x3dtimeslide'}, 'isMobile': false, 'title': '', 'description': '', 'url': '', 'type': 'feed', 'isSingleItem': false, 'isMultipleItems': true, 'isError': false, 'isPage': false, 'isPost': false, 'isHomepage': true, 'isArchive': false, 'isLabelSearch': false}}]);
+    _WidgetManager._RegisterWidget('_BlogView', new _WidgetInfo('Blog1', 'main', document.getElementById('Blog1'), {'cmtInteractionsEnabled': false, 'navMessage': 'Tidak ada postingan.', 'lightboxEnabled': true, 'lightboxModuleUrl': 'https://www.blogger.com/static/v1/jsbin/1400250419-lbx.js', 'lightboxCssUrl': 'https://www.blogger.com/static/v1/v-css/368954415-lightbox_bundle.css'}, 'displayModeFull'));
+    _WidgetManager._RegisterWidget('_HTMLView', new _WidgetInfo('HTML2', 'main', document.getElementById('HTML2'), {}, 'displayModeFull'));
+    <\/script>
+    <\/body>--><\/HTML>
+    /*]]>*/
+    </style>
 
 <title>MSF tv</title>
 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"></link>
@@ -18,15 +48,12 @@ background:white;
 color:black;
 }
 #Navbar1 {dispaly:none!important;}
-
 --></style>
 <link href="https://www.blogger.com/dyn-css/authorization.css?targetBlogID=9052138879193408892&amp;zx=0c87fb2e-7b55-48a5-bd14-fc79434da986" media="none" onload="if(media!='all')media='all'" rel="stylesheet"></link><noscript><link href='https://www.blogger.com/dyn-css/authorization.css?targetBlogID=9052138879193408892&amp;zx=0c87fb2e-7b55-48a5-bd14-fc79434da986' rel='stylesheet'/></noscript>
-
 </head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
 <script src="https://jwpsrv.com/library/5V3tOP97EeK2SxIxOUCPzg.js"></script>
 <style>
-
 /* fonts */
 body { font-weight: normal; font-size: 14px; font-family: Tahoma; }
 .smallfont, .wp-caption-text { font-weight: normal; font-size: 13px; font-family: Tahoma; }
@@ -40,36 +67,28 @@ h3, .h3, .archive-style-3 .posts-list .article .entry-title { font-weight: norma
     background-color: #FF2E2E;
     color: #F6FCF8;
 }
-
 .media-control[data-media-control] .media-control-background[data-background]::before {
 font-size:2vw;color:#fff;text-align:center;    font-family:Segoe UI;font-weight:bold;
 content:" شاهد جميع المباريات علي موقعنا || www.msftv.ga ";  ;  text-shadow: 1px 1px 1px #000;
 }
-
 .media-control[data-media-control].media-control-hide .media-control-background[data-background] {
     opacity: 1!important;
 }
 .media-control[data-media-control] .media-control-background[data-background] {
     height: 6%!important;
 }
-
 .media-control[data-media-control] .media-control-layer[data-controls]{height:25px!important;background-color:rgba(0,0,0,.31)}.media-control[data-media-control] .media-control-layer[data-controls] .bar-container[data-seekbar]{top:-14px}
 .container[data-container] span{display:none}
-
-
 .dvr-controls[data-dvr-controls]{line-height:25px!important}
 .media-control[data-media-control] .media-control-layer[data-controls] .drawer-container[data-volume]{    margin-top: -2px!important;float: left!important;margin-right: 0!important;}
-
 .dvr-controls[data-dvr-controls] .live-info:before{width:6px;height:6px;margin-left:10px}.media-control[data-media-control] .media-control-layer[data-controls] .bar-container[data-seekbar] .bar-background[data-seekbar]{height:3px}
 .media-control[data-media-control] .media-control-layer[data-controls] button.media-control-button[data-hd-indicator] svg{opacity:0}
 .facebook:hover,.image-container:hover,.media-control[data-media-control] .media-control-icon{opacity:1 !important}.media-control[data-media-control]{display:block!important}
-
 .media-control[data-media-control] .media-control-layer[data-controls] button.media-control-button[data-hd-indicator] {
     background: url(https://www.appcreator24.com/srv/imgs/gen/1099105_splash.png) center center/70% no-repeat;
     width: 105px;
     display: block !important;
 }
-
 .chromecast-button,.media-control-indicator[data-duration]{display:none}.extra-padding{z-index:9999;display:inline-block;margin:10px auto 5px;left:0;right:0}.close-padding{color:#fff;cursor:pointer;margin:0;padding-bottom:10px;padding-right:10px;position:absolute;left:0;top:0;z-index:1001}.image-container{opacity:.8;transition:all .3s;display:block;overflow:hidden;background-color:rgba(255,255,255,.11)}.facebook{z-index:70;right:10px;opacity:.4;transition:all .3s}
 .media-control[data-media-control] .media-control-layer[data-controls] .drawer-container[data-volume] .drawer-icon-container[data-volume] {
 background-image: url('data:image/svg+xml;utf8,<svg height='100%' version='1.1' viewBox='0 0 36 36' width='100%' xmlns='http://www.w3.org/2000/svg' xmlns:svg='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>  <path d='M8,21 L12,21 L17,26 L17,10 L12,15 L8,15 L8,21 Z M19,14 L19,22 C20.48,21.32 21.5,19.77 21.5,18 C21.5,16.26 20.48,14.74 19,14 ZM19,11.29 C21.89,12.15 24,14.83 24,18 C24,21.17 21.89,23.85 19,24.71 L19,26.77 C23.01,25.86 26,22.28 26,18 C26,13.72 23.01,10.14 19,9.23 L19,11.29 Z' fill='#fff' id='path7'></path></svg>');}
@@ -109,7 +128,35 @@ else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
 document.oncontextmenu=new Function("return false")
 // -->
 </script>
-
+</div>
+<div class="clear">
+</div>
+<span class="widget-item-control">
+<span class="item-control blog-admin">
+<a class="quickedit" href="https://www.blogger.com/rearrange?blogID=9052138879193408892&amp;widgetType=HTML&amp;widgetId=HTML1&amp;action=editWidget&amp;sectionId=main" onclick="return _WidgetManager._PopupConfig(document.getElementById(&quot;HTML1&quot;));" rel="nofollow" target="configHTML1" title="تعديل">
+<img alt="" height="18" src="https://resources.blogblog.com/img/icon18_wrench_allbkg.png" width="18" />
+</a>
+</span>
+</span>
+<div class="clear">
+</div>
+</div>
+<div class="widget Blog" data-version="1" id="Blog1">
+<div class="blog-posts hfeed">
+          <div class="date-outer">
+        
+          <div class="date-posts">
+        
+<div class="post-outer">
+<div class="post hentry uncustomized-post-template" itemprop="blogPost" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">
+<a href="https://www.blogger.com/null" name="2790089400061449632"></a>
+<h3 class="post-title entry-title" itemprop="name">
+beinlive4u
+</h3>
+<div class="post-header">
+<div class="post-header-line-1">
+</div>
+</div>
 <div class="post-body entry-content" id="post-body-2790089400061449632" itemprop="description articleBody">
 <div dir="rtl" style="text-align: right;" trbidi="on">
 <script src="//api.peer5.com/peer5.js?id=Orxbp2b5gI8"></script>
@@ -129,7 +176,5 @@ player.load({source: topVideo, mimeType: 'video/mp4'});
 player.play();
 //]]>
 </script>
-
-
 </body>
 </html></div>
