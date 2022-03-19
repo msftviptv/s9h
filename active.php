d207941d887c463c72e41e08721e6dd1dec49fea
msftviptv <64074648+msftviptv@users.noreply.github.com>
2022-03-19 11:16:32 +0200

    Create active.php

---
+++ active.php
@@ -0,0 +1,2 @@
+
+["demo-sportgo.blogspot.com","www.9kora.com","yalla-shoot-arab.com","matchelive.com","www.halla-koora-live.com","www.mslsport.info","www.ab7hmaad.com","4803158478776619733","www.asmae-kooora.com","127.0.0.1","www.daily-maroc.com","s9hlive.herokuapp.com","filmala3ab.com","www.newofgames.com","demo.anubis-dev.com","www.yallashooat.com","yalla.gatfilgoal.com","console.log('Go');"]
